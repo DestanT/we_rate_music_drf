7bcb1d6dfd1a3431e6d7785cdff02cec5e460eba
Destan <destantezel@googlemail.com>
2024-01-16 12:17:02 +0000

    Create 'profiles' app and Profile model with a post_save signal on User

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to='we-rate-music/profile', default='../default_profile_lcovgw'
+    )
+    background = models.ImageField(
+        upload_to='we-rate-music/background', default='../default_post_uq5gxz'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.user}'s profile"
+
+
+# Credit: code from Code Institute's React walkthrough project
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+post_save.connect(create_profile, sender=User)
