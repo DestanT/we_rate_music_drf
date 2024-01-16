974914804713d2d605981fbc843d25f7f8e5902e
Destan <destantezel@googlemail.com>
2024-01-16 22:21:52 +0000

    Create ProfileDetail view and update ProfileList view to a generic view

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,24 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from we_rate_music_drf.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 # Credit: code from Code Institute's React walkthrough project
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view, as profile creation is handled by Django signals.
+    """
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+# Credit: code from Code Institute's React walkthrough project
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile, if you're the owner.
+    """
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
