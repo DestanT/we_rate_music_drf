b44c41d487d70ac0be2a20992a15b2bafa5b8b45
Destan <destantezel@googlemail.com>
2024-01-17 21:56:43 +0000

    Add unique_together property to Rating model - one rating per playlist, per user

--- ratings/models.py
+++ ratings/models.py
@@ -18,3 +18,6 @@ class Rating(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ['owner', 'playlist']
