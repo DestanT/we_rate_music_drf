79b7da118b7f4a9c1d2c9a6f79ff7f55ef267d60
Destan <destantezel@googlemail.com>
2024-01-17 21:57:22 +0000

    Add RatingList and Detail views

--- ratings/views.py
+++ ratings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from we_rate_music_drf.permissions import IsOwnerOrReadOnly
+from .models import Rating
+from .serializers import RatingSerializer, RatingDetailSerializer
 
-# Create your views here.
+
+class RatingList(generics.ListCreateAPIView):
+    serializer_class = RatingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Rating.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class RatingDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RatingDetailSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Rating.objects.all()
