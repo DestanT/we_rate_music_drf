d9d92e8a3958edde61d90cbff1b5fcab9f2d986c
Destan <destantezel@googlemail.com>
2024-01-17 18:33:24 +0000

    Create playlist model

--- playlists/models.py
+++ playlists/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Playlist(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    image = models.URLField(max_length=255)
+    url = models.URLField(max_length=255)
+    iframe_uri = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['-added_on']
+
+    def __str__(self):
+        return f"{self.title}"
