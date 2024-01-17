c65fdf954a1c42c88761e071f08d5eacf1899ea3
Destan <destantezel@googlemail.com>
2024-01-17 19:09:22 +0000

    Create Rating model and setup .score with Min and MaxValueValidators

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
+from playlists.models import Playlist
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Rating(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    score = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
