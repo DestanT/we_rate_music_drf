39b462788c8af078c3bb84734219ef878760e82c
Destan <destantezel@googlemail.com>
2024-01-18 21:29:23 +0000

    Create detail and list views for followers

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from we_rate_music_drf.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+# Credit: code from Code Institute's React walkthrough project
+class FollowerList(generics.ListCreateAPIView):
+    serializer_class = FollowerSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = FollowerSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
