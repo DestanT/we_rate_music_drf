df4c50af8f9de479dee9d2ab838c2bce1ca7ac68
Destan <destantezel@googlemail.com>
2024-01-18 17:18:47 +0000

    Create 'Follower' model

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+# Credit: code from Code Institute's React walkthrough project
+class Follower(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed_by')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f"{self.owner} now follows {self.followed}"
