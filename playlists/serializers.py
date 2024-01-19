df47730aa63d0e0462389f78217ec0a6c2016e7a
Destan <destantezel@googlemail.com>
2024-01-19 14:48:36 +0000

    Add puser's rating_id to PlaylistSerializer

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -1,18 +1,29 @@
 from rest_framework import serializers
 from .models import Playlist
+from ratings.models import Rating
 
 
 class PlaylistSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    rating_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
     
+    def get_rating_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            rating = Rating.objects.filter(
+                owner=user, playlist=obj
+            ).first()
+            return rating.id if rating else None
+        return None
+    
     class Meta:
         model = Playlist
         fields = [
             'id', 'owner', 'added_on', 'title', 'image',
-            'is_owner', 'url', 'iframe_uri',
+            'is_owner', 'url', 'iframe_uri', 'rating_id',
         ]
