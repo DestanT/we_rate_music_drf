790cd37a0a13b13621e5958f62ab4353a6bcfe26
Destan <destantezel@googlemail.com>
2024-01-19 19:37:10 +0000

    Display created_at and updated_at in RatingSerializer using naturaltime

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from django.db import IntegrityError
 from .models import Rating
@@ -5,12 +6,20 @@ from .models import Rating
 
 class RatingSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
     playlist_owner = serializers.ReadOnlyField(source='playlist.owner.username')
+    is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
     
     class Meta:
         model = Rating
