11458a7d7d9628c4a40527dee0570a34080a4e68
Destan <destantezel@googlemail.com>
2024-01-19 15:37:58 +0000

    Count amount of ratings given to a Playlist object and calculate the average_rating of each

--- playlists/views.py
+++ playlists/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count, Avg
+from rest_framework import generics, filters, permissions
 from we_rate_music_drf.permissions import IsOwnerOrReadOnly
 from .models import Playlist
 from .serializers import PlaylistSerializer
@@ -7,7 +8,16 @@ from .serializers import PlaylistSerializer
 class PlaylistList(generics.ListCreateAPIView):
     serializer_class = PlaylistSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Playlist.objects.all()
+    queryset = Playlist.objects.annotate(
+        ratings_count = Count('ratings', distinct=True),
+        average_rating = Avg('ratings__score')
+    ).order_by('-added_on')
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        'ratings_count',
+        'average_rating',
+        'ratings__created_at'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -16,4 +26,7 @@ class PlaylistList(generics.ListCreateAPIView):
 class PlaylistDetail(generics.RetrieveDestroyAPIView):
     serializer_class = PlaylistSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Playlist.objects.all()
+    queryset = Playlist.objects.annotate(
+        ratings_count = Count('ratings', distinct=True),
+        average_rating = Avg('ratings__score')
+    )
