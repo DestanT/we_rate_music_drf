216efd20a906e69fbb43d6aab3c13b85daafb822
Destan <destantezel@googlemail.com>
2024-01-24 17:29:15 +0000

    Import NavLink and connect the FontAwesome icons to the routes

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,10 @@
+import styles from '../styles/NavBar.module.css';
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpotify } from '@fortawesome/free-brands-svg-icons';
 import { faStar, faPeopleGroup, faSquare } from '@fortawesome/free-solid-svg-icons';
-import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
@@ -11,49 +12,49 @@ const NavBar = () => {
     <Container>
 
       <Nav className='mr-auto'>
-        <Nav.Link
-          href='#ratings'
+        <NavLink
+          to='/popular'
         >
           {/* Star icon */}
           <FontAwesomeIcon icon={faStar} size='2xl' />
-        </Nav.Link>
+        </NavLink>
       </Nav>
 
       <Nav className='mr-auto'>
-        <Nav.Link
-          href='#followed'
+        <NavLink
+          to='/feed'
         >
           {/* Group/Follow icon */}
           <FontAwesomeIcon icon={faPeopleGroup} size='2xl' />
-        </Nav.Link>
+        </NavLink>
       </Nav>
 
       <Nav>
         {/* <Navbar.Brand href='#my-profile'>We Rate Music</Navbar.Brand> DELETE/CHANGE */}
-        <Nav.Link
-          href='#my-profile'
+        <NavLink
+          to='/profile'
         >
           {/* Profile image */}
           <FontAwesomeIcon icon={faSquare} size='2xl' />
-        </Nav.Link>
+        </NavLink>
       </Nav>
 
       <Nav className='ml-auto'>
-        <Nav.Link
-          href='#placeholder'
+        <NavLink
+          to='/placeholder'
         >
           {/* Placeholder - ^^ IMPLEMENT IF/ELSE FOR PROFILE IMAGE FOR NAV.LINK/NAVBAR.BRAND ^^ */}
           <FontAwesomeIcon icon={faSquare} size='2xl' />
-        </Nav.Link>
+        </NavLink>
       </Nav>
 
       <Nav className='ml-auto'>
-        <Nav.Link
-          href='#spotify'
+        <NavLink
+          to='/spotify-search'
         >
           {/* Spotify link */}
           <FontAwesomeIcon icon={faSpotify} style={{color: '#1db954',}} size='2xl' />
-        </Nav.Link>
+        </NavLink>
       </Nav>
       
     </Container>
