45f6c61b2f3eef85ad1cdd18dc19b847096176b4
Destan <destantezel@googlemail.com>
2024-01-24 17:28:23 +0000

    IAdd Ra Switch and Routes from react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
+import styles from './App.module.css';
 import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home Page</h1>} />
+          <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
+          <Route exact path='/profile' render={() => <h1>Profile</h1>} />
+          <Route exact path='/popular' render={() => <h1>Popular</h1>} />
+          <Route exact path='/feed' render={() => <h1>Feed</h1>} />
+          <Route exact path='/spotify-search' render={() => <h1>Spotify Search</h1>} />
+          <Route exact path='/placeholder' render={() => <h1>Placeholder</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
