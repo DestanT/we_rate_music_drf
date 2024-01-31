2f38ce502ba92a4d540042cc7908aabdc4f1bf36
Destan <destantezel@googlemail.com>
2024-01-31 20:43:40 +0000

    Change the overall layout of the Playlist component and delete unnecessary style imports

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/Playlist.module.css';
+import { Col } from 'react-bootstrap';
 
 const Playlist = (props) => {
   const { title, image, url, iframe_uri } = props;
   const defaultImage =
     'https://res.cloudinary.com/dxgzepuov/image/upload/v1702896303/default_post_uq5gxz.jpg';
   return (
-    <div className='media'>
+    <Col xs={12} md={4} lg={3} className={styles.Playlist}>
       <img
         src={image || defaultImage}
-        className={`mr-3 ${styles.Playlist}`}
-        alt='...'
+        className={styles.Image}
+        alt={`${title}'s cover art`}
       />
-      <div className='media-body'>
-        <h5 className='mt-0'>{title}</h5>
-        <p>{url}</p>
-        <p>{iframe_uri}</p>
-      </div>
-    </div>
+      <h5 className='mt-0'>{title}</h5>
+    </Col>
   );
 };
 
