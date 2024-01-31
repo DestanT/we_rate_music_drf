eb445111d2a6a65e64842b0d7a6c06e51b20b6ff
Destan <destantezel@googlemail.com>
2024-01-31 14:17:31 +0000

    Update Playlist component to display Spotify search results

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -1,34 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { Card } from 'react-bootstrap';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
 import styles from '../styles/PlaylistsPage.module.css';
 
 const Playlist = (props) => {
-  const currentUser = useCurrentUser();
-  const {
-    id,
-    owner,
-    added_on,
-    title,
-    image,
-    is_owner,
-    url,
-    iframe_uri,
-    rating_id,
-    ratings_count,
-    average_rating
-  } = props;
+  const { title, image, url, iframe_uri } = props;
   return (
     <Card>
-      <Card.Img variant="top" src={currentUser?.profile_image} className={styles.Playlist} />
+      <Card.Img variant='top' src={image} className={styles.Playlist} />
       <Card.Body>
-        <Card.Title>{currentUser.username}'s Playlist</Card.Title>
-        <Card.Text>
-          Some playlist information
-        </Card.Text>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{url}</Card.Text>
+        <Card.Text>{iframe_uri}</Card.Text>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Playlist
+export default Playlist;
