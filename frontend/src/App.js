f144beb9205c9fe248687a48bc06ef3aedd7e0cd
Destan <destantezel@googlemail.com>
2024-02-01 19:55:50 +0000

    Set up a Route to the ProfilePage and delete <Container> wrapper around the Switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import styles from './App.module.css';
 // import logo from './logo.svg';
 import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
@@ -9,6 +8,7 @@ import SignInForm from './pages/auth/SignInForm';
 import SpotifySearchPage from './pages/playlists/SpotifySearchPage';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 import AccordionWindow from './components/AccordionWindow';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   const currentUser = useCurrentUser();
@@ -18,31 +18,21 @@ function App() {
     <div className={styles.App}>
       <NavBar />
       <AccordionWindow />
-      <Container>
-        <Switch>
-          <Route exact path='/' render={() => <h1>Home Page</h1>} />
-          <Route exact path='/signin' render={() => <SignInForm />} />
-          <Route exact path='/signup' render={() => <SignUpForm />} />
-          <Route
-            exact
-            path='/profile'
-            render={() => <h1>{`Profile of ${profile_username}`}</h1>}
-          />
-          <Route exact path='/popular' render={() => <h1>Popular</h1>} />
-          <Route exact path='/feed' render={() => <h1>Followed Users</h1>} />
-          <Route
-            exact
-            path='/spotify-search'
-            render={() => <SpotifySearchPage />}
-          />
-          <Route
-            exact
-            path='/placeholder'
-            render={() => <h1>Placeholder</h1>}
-          />
-          <Route render={() => <h1>Page Not Found!</h1>} />
-        </Switch>
-      </Container>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Home Page</h1>} />
+        <Route exact path='/signin' render={() => <SignInForm />} />
+        <Route exact path='/signup' render={() => <SignUpForm />} />
+        <Route exact path='/profile' render={() => <ProfilePage />} />
+        <Route exact path='/popular' render={() => <h1>Popular</h1>} />
+        <Route exact path='/feed' render={() => <h1>Followed Users</h1>} />
+        <Route
+          exact
+          path='/spotify-search'
+          render={() => <SpotifySearchPage />}
+        />
+        <Route exact path='/placeholder' render={() => <h1>Placeholder</h1>} />
+        <Route render={() => <h1>Page Not Found!</h1>} />
+      </Switch>
     </div>
   );
 }
