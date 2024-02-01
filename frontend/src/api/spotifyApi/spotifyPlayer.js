4967113742250c7ec9ed6c8d2be50aa88549859c
Destan <destantezel@googlemail.com>
2024-02-01 12:59:39 +0000

    Update the Spotify iFrame player with URI context and re-render the  new playlist in player

--- frontend/src/api/spotifyApi/spotifyPlayer.js
+++ frontend/src/api/spotifyApi/spotifyPlayer.js
@@ -1,8 +1,11 @@
 // SOURCE: https://developer.spotify.com/documentation/embeds/tutorials/using-the-iframe-api
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useSpotifyPlayerUri } from '../../contexts/SpotifyPlayerUriContext';
 
 function SpotifyPlayer() {
   const playerRef = useRef();
+  const spotifyPlayerUri = useSpotifyPlayerUri();
+  const [embedController, setEmbedController] = useState(null);
 
   useEffect(() => {
     const script = document.createElement('script');
@@ -14,14 +17,22 @@ function SpotifyPlayer() {
       const options = {
         width: '100%',
         height: '250px',
-        uri: 'spotify:episode:7makk4oTQel546B0PZlDM5',
+        uri: spotifyPlayerUri,
       };
 
-      const callback = (EmbedController) => {};
+      const callback = (EmbedController) => {
+        setEmbedController(EmbedController);
+      };
 
       IFrameAPI.createController(playerRef.current, options, callback);
     };
   }, []);
+
+  useEffect(() => {
+    if (embedController) {
+      embedController.loadUri(spotifyPlayerUri);
+    }
+  }, [spotifyPlayerUri, embedController]);
 
   return <div ref={playerRef}></div>;
 }
