f9171344291837ef9b4df3b3f91fc064e5f2918d
Destan <destantezel@googlemail.com>
2024-02-01 12:57:37 +0000

    Wrap Spotify playlists' URI context around the App in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import { SpotifyPlayerUriProvider } from './contexts/SpotifyPlayerUriContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <CurrentUserProvider>
-      <Router>
-        <App />
-      </Router>
+      <SpotifyPlayerUriProvider>
+        <Router>
+          <App />
+        </Router>
+      </SpotifyPlayerUriProvider>
     </CurrentUserProvider>
   </React.StrictMode>,
   document.getElementById('root')
