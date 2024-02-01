07e179999479c8c24a79fd7476908c3117e0b7cf
Destan <destantezel@googlemail.com>
2024-02-01 12:58:41 +0000

    Set URI context data for playlists when clicked

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import styles from '../styles/Playlist.module.css';
-import { Col } from 'react-bootstrap';
+import { Col, Button } from 'react-bootstrap';
+import { useSetSpotifyPlayerUri } from '../contexts/SpotifyPlayerUriContext';
 
 const Playlist = (props) => {
   const { title, image, url, iframe_uri } = props;
+  const setSpotifyPlayerUri = useSetSpotifyPlayerUri();
+
+  const handleClick = () => {
+    setSpotifyPlayerUri(iframe_uri);
+  };
+
+  // Set default image if no image is provided
   const defaultImage =
     'https://res.cloudinary.com/dxgzepuov/image/upload/v1702896303/default_post_uq5gxz.jpg';
   return (
-    <Col xs={12} md={4} lg={3} className={styles.Playlist}>
-      <img
-        src={image || defaultImage}
-        className={styles.Image}
-        alt={`${title}'s cover art`}
-      />
-      <h5 className='mt-0'>{title}</h5>
-    </Col>
+    <Button variant='link' onClick={handleClick}>
+      <Col xs={12} md={4} lg={3} className={styles.Playlist}>
+        <img
+          src={image || defaultImage}
+          className={styles.Image}
+          alt={`${title}'s cover art`}
+        />
+        <h5 className='mt-0'>{title}</h5>
+      </Col>
+    </Button>
   );
 };
 
