73c44a4f520de4329c7f23301d357a736cace8f9
Destan <destantezel@googlemail.com>
2024-02-01 23:30:03 +0000

    Fetch the profile data using the ID in the url - pass data on as prop to Profile

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Profile from '../components/Profile';
+import { useParams } from 'react-router-dom';
+import { axiosReq } from '../api/axiosDefaults';
 
 const ProfilePage = () => {
-  return <Profile />;
+  const { id } = useParams();
+  const [profileData, setProfileData] = useState(null);
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const [{ data }] = await Promise.all([axiosReq.get(`profiles/${id}`)]);
+        setProfileData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProfileData();
+    console.log('profileData: ', profileData);
+  }, [id]);
+
+  return <Profile data={profileData} />;
 };
 
 export default ProfilePage;
