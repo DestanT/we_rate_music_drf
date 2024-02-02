85f67f57f308502c2b880897078148b45a183360
Destan <destantezel@googlemail.com>
2024-02-02 22:40:15 +0000

    Fetch playlists from drf api and map over Playlist components to display them

--- frontend/src/pages/playlists/PlaylistsPage.js
+++ frontend/src/pages/playlists/PlaylistsPage.js
@@ -1,42 +1,40 @@
-import React from 'react'
-import styles from '../../styles/PlaylistsPage.module.css'
-import { Col, Row } from 'react-bootstrap'
-import { useCurrentUser } from '../../contexts/CurrentUserContext'
+import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import Playlist from '../../components/Playlist';
+import { axiosReq } from '../../api/axiosDefaults';
+import LoadingSpinner from '../../components/LoadingSpinner';
 
 const PlaylistsPage = () => {
   const currentUser = useCurrentUser();
-  return (
-    <Row>
-      <Col xs={4} xl={3}>
-        <img
-        className={styles.Playlist}
-        src={currentUser?.profile_image}
-        alt={`${currentUser?.username}' playlist`}
-        />
-      </Col>
-      <Col xs={4} xl={3}>
-        <img
-        className={styles.Playlist}
-        src={currentUser?.profile_image}
-        alt={`${currentUser?.username}' playlist`}
-        />
-      </Col>
-      <Col xs={4} xl={3}>
-        <img
-        className={styles.Playlist}
-        src={currentUser?.profile_image}
-        alt={`${currentUser?.username}' playlist`}
-        />
-      </Col>
-      <Col xs={4} xl={3}>
-        <img
-        className={styles.Playlist}
-        src={currentUser?.profile_image}
-        alt={`${currentUser?.username}' playlist`}
-        />
-      </Col>
-    </Row>
-  )
-}
+  const [playlists, setPlaylists] = useState([]);
+  const [hasLoaded, setHasLoaded] = useState(false);
 
-export default PlaylistsPage
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      try {
+        const { data } = await axiosReq.get('playlists/');
+        setPlaylists(data.results);
+        console.log(data.results);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPlaylists();
+  }, []);
+
+  return hasLoaded ? (
+    <Row>
+      {playlists.map((playlist) => (
+        <Playlist key={playlist.id} playlist={playlist} />
+      ))}
+    </Row>
+  ) : (
+    <LoadingSpinner />
+  );
+};
+
+export default PlaylistsPage;
