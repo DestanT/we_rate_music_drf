0f22f4f13bc6586cb91885ff3f08446a2b8e2074
Destan <destantezel@googlemail.com>
2024-02-05 19:13:07 +0000

    Create fetchMoreData function to  get more data from API when paginated

--- frontend/src/utils/dataUtils.js
+++ frontend/src/utils/dataUtils.js
@@ -1,3 +1,29 @@
+import { axiosReq } from '../api/axiosDefaults';
+
+/**
+ * Fetches more data from a paginated API and updates the state of the resource.
+ * CREDIT: Code taken from Code Institute's 'Moments' walkthrough project
+ *
+ * @param {Object} resource - resource to be updated
+ * @param {Function} setResource - function to update the resource
+ */
+export const fetchMoreData = async (resource, setResource) => {
+  try {
+    const { data } = await axiosReq.get(resource.next);
+    setResource((prevResource) => ({
+      ...prevResource,
+      next: data.next,
+      results: data.results.reduce((acc, curr) => {
+        return acc.some((accResult) => accResult.id === curr.id)
+          ? acc
+          : [...acc, curr];
+      }, prevResource.results),
+    }));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 /**
  * Normalises data from the Spotify API to match the format of DRF's API
  *
