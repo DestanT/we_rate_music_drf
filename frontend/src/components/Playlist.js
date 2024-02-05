26d2530abdb59ec42f937b194d7f242b0bae7ba6
Destan <destantezel@googlemail.com>
2024-02-05 16:38:25 +0000

    Delete <Button> wrapping and click handler, and move to parent element - separation of concerns

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from '../styles/Playlist.module.css';
-import { Button } from 'react-bootstrap';
-import { useSetSpotifyPlayerUri } from '../contexts/SpotifyPlayerUriContext';
 
 const Playlist = ({ data }) => {
   const {
@@ -21,26 +19,19 @@ const Playlist = ({ data }) => {
     ratings_count,
   } = data;
 
-  const setSpotifyPlayerUri = useSetSpotifyPlayerUri();
-
   // Set default image if no image is provided
   const defaultImage =
     'https://res.cloudinary.com/dxgzepuov/image/upload/v1702896303/default_post_uq5gxz.jpg';
 
-  // Set the Spotify player's URI for SpotifyPlayer.js component
-  const handleClick = () => {
-    setSpotifyPlayerUri(iframe_uri);
-  };
-
   return (
-    <Button variant='link' onClick={handleClick} className={styles.Button}>
+    <>
       <img
         src={image || defaultImage}
         className={styles.Image}
         alt={`${title}'s cover art`}
       />
       <h5 className='mt-0'>{title}</h5>
-    </Button>
+    </>
   );
 };
 
