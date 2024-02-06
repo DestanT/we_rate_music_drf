709d0a4abc1819cc82513a253335201f38652f3c
Destan <destantezel@googlemail.com>
2024-02-06 17:09:57 +0000

    Refactor Playlist component - defaultImage now declared outside of the main component function

--- frontend/src/components/Playlist.js
+++ frontend/src/components/Playlist.js
@@ -1,36 +1,23 @@
 import React from 'react';
 import styles from '../styles/Playlist.module.css';
 
-const Playlist = ({ data }) => {
-  const {
-    // Spotify data
-    spotify_id,
-    title,
-    image,
-    url,
-    iframe_uri,
-    // DRF data
-    id,
-    owner,
-    is_owner,
-    added_on,
-    average_rating,
-    rating_id,
-    ratings_count,
-  } = data;
+// Set default image if no image is provided
+const defaultImage =
+  'https://res.cloudinary.com/dxgzepuov/image/upload/v1702896303/default_post_uq5gxz.jpg';
 
-  // Set default image if no image is provided
-  const defaultImage =
-    'https://res.cloudinary.com/dxgzepuov/image/upload/v1702896303/default_post_uq5gxz.jpg';
-
+/**
+ * Playlist component to display the image of a playlist. Default image is used if no image is provided.
+ *
+ * @param {string} image - The image URL of the playlist
+ * @param {string} title - The title of the playlist
+ */
+const Playlist = ({ image, title }) => {
   return (
-    <>
-      <img
-        src={image || defaultImage}
-        className={styles.Image}
-        alt={`${title}'s cover art`}
-      />
-    </>
+    <img
+      src={image || defaultImage}
+      className={styles.Image}
+      alt={`${title}'s cover art`}
+    />
   );
 };
 
