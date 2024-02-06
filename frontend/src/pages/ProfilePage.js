4eac4ea19951e16502aec2dadae721f8972cf3aa
Destan <destantezel@googlemail.com>
2024-02-06 12:30:14 +0000

    Update ProfilePage.js with new path parameters

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import Profile from '../components/Profile';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { axiosReq } from '../api/axiosDefaults';
+
+import PlaylistsPage from './playlists/PlaylistsPage';
+import Profile from '../components/Profile';
 
 const ProfilePage = () => {
-  return <Profile />;
+  const { userId } = useParams();
+  return (
+    <>
+      <Profile userId={userId} />
+      <PlaylistsPage filter={`owner__profile=${userId}`} />
+    </>
+  );
 };
 
 export default ProfilePage;
