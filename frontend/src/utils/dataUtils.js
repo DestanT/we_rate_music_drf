0cd334db9804441fa2bde6d1c96bfdc574cbf5b9
Destan <destantezel@googlemail.com>
2024-02-09 11:15:17 +0000

    Import jwtDecode and create functions to handle setting and removing token timestamps

--- frontend/src/utils/dataUtils.js
+++ frontend/src/utils/dataUtils.js
@@ -1,8 +1,9 @@
+// CREDIT: Code taken from Code Institute's 'Moments' walkthrough project
 import { axiosReq } from '../api/axiosDefaults';
+import jwtDecode from 'jwt-decode';
 
 /**
  * Fetches more data from a paginated API and updates the state of the resource.
- * CREDIT: Code taken from Code Institute's 'Moments' walkthrough project
  *
  * @param {Object} resource - resource to be updated
  * @param {Function} setResource - function to update the resource
@@ -22,4 +23,17 @@ export const fetchMoreData = async (resource, setResource) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+export const setTokenTimestamp = (data) => {
+  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  localStorage.setItem('refreshTokenTimestamp', refreshTokenTimestamp);
+};
+
+export const shouldRefreshToken = () => {
+  return !!localStorage.getItem('refreshTokenTimestamp');
+};
+
+export const removeTokenTimestamp = () => {
+  localStorage.removeItem('refreshTokenTimestamp');
 };
