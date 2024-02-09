f146d2310c5785dc0b33dc00d7eaa5276c091484
Destan <destantezel@googlemail.com>
2024-02-09 11:09:14 +0000

    Delete normaliseSpotifyData function from dataUtils.js - unused

--- frontend/src/utils/dataUtils.js
+++ frontend/src/utils/dataUtils.js
@@ -23,27 +23,3 @@ export const fetchMoreData = async (resource, setResource) => {
     console.log(err);
   }
 };
-
-/**
- * Normalises data from the Spotify API to match the format of DRF's API
- *
- * @param {Object} - data from the Spotify API
- * @returns {Object} - normalised data for the Playlist component
- */
-export function normaliseSpotifyData(data) {
-  return {
-    spotify_id: data.id,
-    title: data.name,
-    image: data.images[0]?.url,
-    url: data.external_urls.spotify,
-    iframe_uri: data.uri,
-
-    id: null,
-    owner: null,
-    is_owner: null,
-    added_on: null,
-    average_rating: null,
-    rating_id: null,
-    ratings_count: null,
-  };
-}
