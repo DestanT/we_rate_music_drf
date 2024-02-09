f5554b4c5a8d604539fed63e64af5c7235db9c4b
Destan <destantezel@googlemail.com>
2024-02-09 01:14:02 +0000

    Add useRedirect hook to ProfilePage

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import { useRedirect } from '../hooks/useRedirect';
+
 import PlaylistsPage from './PlaylistsPage';
 import Profile from '../components/Profile';
 
 const ProfilePage = () => {
   const { userId } = useParams();
+  useRedirect();
   return (
     <>
       <Profile userId={userId} />
