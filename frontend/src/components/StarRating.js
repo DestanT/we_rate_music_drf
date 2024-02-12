41a206cd7e20dc073fe5492929f68cfd7b85a544
Destan <destantezel@googlemail.com>
2024-02-12 13:09:10 +0000

    Create StarRating component to handle .get, .post, .put for the the Rating model

---
+++ frontend/src/components/StarRating.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { Rating, StickerStar } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css';
+import { axiosReq, axiosRes } from '../api/axiosDefaults';
+
+const StarRating = ({ playlist }) => {
+  const [rating, setRating] = useState(0);
+  console.log(playlist);
+
+  useEffect(() => {
+    const fetchRatingData = async () => {
+      if (playlist.rating_id) {
+        try {
+          const { data } = await axiosRes.get(`ratings/${playlist.rating_id}`);
+          setRating(data.score);
+          console.log('rating data: ', data);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    };
+    fetchRatingData();
+  }, [playlist]);
+
+  const handleChange = async (selectedValue) => {
+    if (playlist.rating_id) {
+      try {
+        await axiosReq.put(`ratings/${playlist.rating_id}`, {
+          score: selectedValue,
+        });
+
+        setRating(selectedValue);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      try {
+        await axiosReq.post('ratings/', {
+          playlist: playlist.id,
+          score: selectedValue,
+        });
+
+        setRating(selectedValue);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const myStyles = {
+    itemShapes: StickerStar,
+    activeFillColor: '#df604e',
+    inactiveFillColor: '#df5f4e6e',
+  };
+
+  return (
+    <Rating
+      value={rating}
+      onChange={handleChange}
+      style={{ maxWidth: 250 }}
+      itemStyles={myStyles}
+      items={5}
+    />
+  );
+};
+
+export default StarRating;
