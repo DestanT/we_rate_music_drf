980fdfcb52bceed1f474281c6d1a3a677b7603fa
Destan <destantezel@googlemail.com>
2024-02-15 15:00:24 +0000

    Added max-width properties to title for a more uniform look across different screen sizes

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -8,11 +8,11 @@ const Homepage = () => {
       <Row>
         <Col xs={7}></Col>
         <Col xs={5} className={styles.TitleContainer}>
-          <h1>We Rate Music</h1>
+          <h1 className={styles.Title}>We Rate Music</h1>
           <hr className={styles.Line} />
           <hr className={styles.SecondLine} />
-          <Row className={styles.PaddingTop}>
-            <Col>
+          <Row>
+            <Col className={styles.IntroContainer}>
               <p>
                 Welcome to We Rate Music! The place to rate and review your
                 favorite music.
