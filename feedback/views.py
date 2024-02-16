ee9db1f6a837d01004accadbe7650f087bc8ad68
Destan <destantezel@googlemail.com>
2024-02-16 18:33:53 +0000

    DRF: add Feedback list and details views - list create, and retrieve destroy respectively

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from we_rate_music_drf.permissions import IsOwnerOrReadOnly
+from .models import Feedback
+from .serializers import FeedbackSerializer
 
-# Create your views here.
+
+class FeedbackList(generics.ListCreateAPIView):
+    serializer_class = FeedbackSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Feedback.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class FeedbackDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = FeedbackSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Feedback.objects.all()
