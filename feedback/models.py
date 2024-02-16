fbd57885bb713f7196fb8e9e4adf91d6dd49531d
Destan <destantezel@googlemail.com>
2024-02-16 18:03:22 +0000

    DRF: create the feedback model

--- feedback/models.py
+++ feedback/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    feedback = models.TextField(blank=False, null=False)
+    image = models.ImageField(
+        upload_to='we-rate-music/feedback', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.user}'s feedback, titled: {self.title}"
