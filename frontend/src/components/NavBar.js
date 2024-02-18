95aa68b1e1b068be5a99dda53e4353f08c8d93cb
Destan <destantezel@googlemail.com>
2024-02-18 00:55:36 +0000

    Add  tooltips to all navlinks

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,12 @@
 import styles from '../styles/NavBar.module.css';
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import {
+  Navbar,
+  Container,
+  Nav,
+  OverlayTrigger,
+  Tooltip,
+} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpotify } from '@fortawesome/free-brands-svg-icons';
 import {
@@ -19,81 +25,126 @@ const NavBar = () => {
 
   const loggedOutIcons = (
     <>
-      <Navbar.Brand className={styles.Brand} href='/'>
-        We -R- Music
-      </Navbar.Brand>
+      {/* Brand logo */}
+      <OverlayTrigger
+        placement='bottom'
+        overlay={<Tooltip id='feedback-tooltip'>Home</Tooltip>}
+      >
+        <Navbar.Brand className={styles.Brand} href='/'>
+          We -R- Music
+        </Navbar.Brand>
+      </OverlayTrigger>
+
       <Nav className={styles.SignInOut}>
-        <NavLink
-          className={styles.SignUpIn}
-          activeClassName={styles.Active}
-          to='/signin'
+        {/* Login button */}
+        <OverlayTrigger
+          placement='bottom'
+          overlay={<Tooltip id='feedback-tooltip'>Click to Login</Tooltip>}
         >
-          {/* Signin icon */}
-          <FontAwesomeIcon
-            className={styles.MarginRight}
-            icon={faRightToBracket}
-            size='2xl'
-          />
-          Login
-        </NavLink>
-        <NavLink
-          className={styles.SignUpIn}
-          activeClassName={styles.Active}
-          to='/signup'
+          <NavLink
+            className={styles.SignUpIn}
+            activeClassName={styles.Active}
+            to='/signin'
+          >
+            <FontAwesomeIcon
+              className={styles.MarginRight}
+              icon={faRightToBracket}
+              size='2xl'
+            />
+            Login
+          </NavLink>
+        </OverlayTrigger>
+
+        {/* Signup button */}
+        <OverlayTrigger
+          placement='bottom'
+          overlay={<Tooltip id='feedback-tooltip'>Click to sign up!</Tooltip>}
         >
-          {/* Signup icon */}
-          <FontAwesomeIcon
-            className={styles.MarginRight}
-            icon={faUserPlus}
-            size='2xl'
-          />
-          Sign Up!
-        </NavLink>
+          <NavLink
+            className={styles.SignUpIn}
+            activeClassName={styles.Active}
+            to='/signup'
+          >
+            <FontAwesomeIcon
+              className={styles.MarginRight}
+              icon={faUserPlus}
+              size='2xl'
+            />
+            Sign Up!
+          </NavLink>
+        </OverlayTrigger>
       </Nav>
     </>
   );
   const loggedInIcons = (
     <>
+      {/* All playlists */}
       <Nav className='mr-auto'>
-        <NavLink activeClassName={styles.Active} to='/global'>
-          {/* All playlists */}
-          <FontAwesomeIcon icon={faGlobe} size='2xl' />
-        </NavLink>
-      </Nav>
-
-      <Nav className='mr-auto'>
-        <NavLink activeClassName={styles.Active} to='/feed'>
-          {/* Followed user playlists */}
-          <FontAwesomeIcon icon={faPeopleGroup} size='2xl' />
-        </NavLink>
-      </Nav>
-
-      <Nav>
-        <NavLink
-          activeClassName={styles.ProfileActive}
-          to={`/profile/${currentUser?.profile_id}`}
+        <OverlayTrigger
+          placement='top'
+          overlay={<Tooltip id='feedback-tooltip'>All playlists</Tooltip>}
         >
-          {/* User's profile */}
-          <Avatar src={currentUser?.profile_image} height={40} />
-        </NavLink>
+          <NavLink activeClassName={styles.Active} to='/global'>
+            <FontAwesomeIcon icon={faGlobe} size='2xl' />
+          </NavLink>
+        </OverlayTrigger>
       </Nav>
 
-      <Nav className='ml-auto'>
-        <NavLink activeClassName={styles.Active} to='/rated-playlists'>
-          {/* User's rated playlists */}
-          <FontAwesomeIcon icon={faStar} size='2xl' />
-        </NavLink>
+      {/* Followed user playlists */}
+      <Nav className='mr-auto'>
+        <OverlayTrigger
+          placement='top'
+          overlay={<Tooltip id='feedback-tooltip'>Followed user feed</Tooltip>}
+        >
+          <NavLink activeClassName={styles.Active} to='/feed'>
+            <FontAwesomeIcon icon={faPeopleGroup} size='2xl' />
+          </NavLink>
+        </OverlayTrigger>
       </Nav>
 
+      {/* User's profile */}
+      <Nav>
+        <OverlayTrigger
+          placement='top'
+          overlay={<Tooltip id='feedback-tooltip'>Your profile</Tooltip>}
+        >
+          <NavLink
+            activeClassName={styles.ProfileActive}
+            to={`/profile/${currentUser?.profile_id}`}
+          >
+            <Avatar src={currentUser?.profile_image} height={40} />
+          </NavLink>
+        </OverlayTrigger>
+      </Nav>
+
+      {/* User's rated playlists */}
       <Nav className='ml-auto'>
-        <NavLink to='/spotify-search'>
-          {/* Spotify search page */}
-          <FontAwesomeIcon
-            icon={faSpotify}
-            style={{ color: '#1db954' }}
-            size='2xl'
-          />
-        </NavLink>
+        <OverlayTrigger
+          placement='top'
+          overlay={
+            <Tooltip id='feedback-tooltip'>Playlists you've rated</Tooltip>
+          }
+        >
+          <NavLink activeClassName={styles.Active} to='/rated-playlists'>
+            <FontAwesomeIcon icon={faStar} size='2xl' />
+          </NavLink>
+        </OverlayTrigger>
+      </Nav>
+
+      {/* Spotify search page */}
+      <Nav className='ml-auto'>
+        <OverlayTrigger
+          placement='top'
+          overlay={<Tooltip id='feedback-tooltip'>Search Spotify</Tooltip>}
+        >
+          <NavLink to='/spotify-search'>
+            <FontAwesomeIcon
+              icon={faSpotify}
+              style={{ color: '#1db954' }}
+              size='2xl'
+            />
+          </NavLink>
+        </OverlayTrigger>
       </Nav>
     </>
   );
