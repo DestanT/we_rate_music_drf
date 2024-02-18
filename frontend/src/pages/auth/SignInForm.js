b5195a580ab861455bcf83cf1f52c75a376ed55c
Destan <destantezel@googlemail.com>
2024-02-18 20:15:03 +0000

    Delete unused image and update SignInForm layout

--- frontend/src/pages/auth/SignInForm.js
+++ frontend/src/pages/auth/SignInForm.js
@@ -1,14 +1,6 @@
 // CREDIT: Code taken from Code Institute's "Moments" walkthrough project
 import React, { useState } from 'react';
-import {
-  Form,
-  Alert,
-  Button,
-  Col,
-  Row,
-  Image,
-  Container,
-} from 'react-bootstrap';
+import { Form, Alert, Button, Col, Row, Container } from 'react-bootstrap';
 
 import axios from 'axios';
 import { Link, useHistory } from 'react-router-dom';
@@ -55,73 +47,66 @@ function SignInForm() {
   };
 
   return (
-    <Row className={styles.Row}>
-      <Col className='my-auto p-0 p-md-2' md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign in</h1>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId='username'>
-              <Form.Label className='d-none'>Username</Form.Label>
-              <Form.Control
-                type='text'
-                placeholder='Username'
-                name='username'
-                className={styles.Input}
-                value={username}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert key={idx} variant='warning'>
-                {message}
-              </Alert>
-            ))}
+    <Container>
+      <Row className={styles.Row}>
+        <Col className='my-auto py-2 p-md-2'>
+          <Container className={`${appStyles.Content} p-4`}>
+            <h1 className={styles.Header}>sign in</h1>
 
-            <Form.Group controlId='password'>
-              <Form.Label className='d-none'>Password</Form.Label>
-              <Form.Control
-                type='password'
-                placeholder='Password'
-                name='password'
-                className={styles.Input}
-                value={password}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password?.map((message, idx) => (
-              <Alert key={idx} variant='warning'>
-                {message}
-              </Alert>
-            ))}
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-              type='submit'
-            >
-              Sign in
-            </Button>
-            {errors.non_field_errors?.map((message, idx) => (
-              <Alert key={idx} variant='warning' className='mt-3'>
-                {message}
-              </Alert>
-            ))}
-          </Form>
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to='/signup'>
-            Don't have an account? <span>Sign up now!</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={'https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg'}
-        />
-      </Col>
-    </Row>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId='username'>
+                <Form.Label className='d-none'>Username</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type='text'
+                  placeholder='Username'
+                  name='username'
+                  value={username}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.username?.map((message, idx) => (
+                <Alert key={idx} variant='warning'>
+                  {message}
+                </Alert>
+              ))}
+
+              <Form.Group controlId='password'>
+                <Form.Label className='d-none'>Password</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type='password'
+                  placeholder='Password'
+                  name='password'
+                  value={password}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {errors.password?.map((message, idx) => (
+                <Alert key={idx} variant='warning'>
+                  {message}
+                </Alert>
+              ))}
+
+              <Button className={btnStyles.Button} type='submit'>
+                Sign in
+              </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                <Alert key={idx} variant='warning' className='mt-3'>
+                  {message}
+                </Alert>
+              ))}
+            </Form>
+          </Container>
+
+          <Container className={`mt-3 ${appStyles.Content}`}>
+            <Link className={styles.Link} to='/signup'>
+              Don't have an account? <span>Sign up now!</span>
+            </Link>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
